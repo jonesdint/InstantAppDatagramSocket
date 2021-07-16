8f9f105a82616e113a05b550a079e88733dfcf01
David Jones <david@intelliware.ca>
2021-07-16 10:57:50 -0400

    Directly set home output

--- app/src/main/java/com/intelliware/instantappdatagramsocket/ui/home/HomeFragment.java
+++ app/src/main/java/com/intelliware/instantappdatagramsocket/ui/home/HomeFragment.java
@@ -22,25 +22,18 @@ import java.net.SocketException;
 public class HomeFragment extends Fragment {
     private static final String TAG = "HomeFragment";
 
-    private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
-        
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        String text = "This is home fragment";
+        textView.setText(text);
 
         openDatagramSocket();
 
